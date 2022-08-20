a484fab1e61aa79c3dfbdb6125874a5cca498123
Simon Tucker <brotherlogic@gmail.com>
2022-08-20 14:52:13 -0700

    Oops

--- recordcleanerapi.go
+++ recordcleanerapi.go
@@ -248,7 +248,9 @@ func (s *Server) GetClean(ctx context.Context, req *pb.GetCleanRequest) (*pb.Get
 		if record.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_PRE_VALIDATE &&
 			record.GetMetadata().GetCategory() != rcpb.ReleaseMetadata_ARRIVED &&
 			yearDayCount >= 5 {
-			return nil, status.Errorf(codes.FailedPrecondition, "you've cleaned %v records today, that is plenty", config.GetDayCount())
+			if time.Now().Weekday() != time.Saturday && time.Now().Weekday() != time.Sunday {
+				return nil, status.Errorf(codes.FailedPrecondition, "you've cleaned %v records today, that is plenty", config.GetDayCount())
+			}
 		}
 
 		return &pb.GetCleanResponse{InstanceId: config.CurrentBoxPick, Seen: sids}, nil

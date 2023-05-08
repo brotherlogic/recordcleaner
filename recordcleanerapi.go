1ef8fbae0708a24d690bb110ada4b5f80b7968c1
Simon Tucker <brotherlogic@gmail.com>
2023-05-08 10:12:57 -0700

    Fix memory leak in record cleaner

--- recordcleanerapi.go
+++ recordcleanerapi.go
@@ -156,10 +156,11 @@ func (s *Server) GetClean(ctx context.Context, req *pb.GetCleanRequest) (*pb.Get
 	if time.Now().Hour() < 8 {
 		return nil, status.Errorf(codes.OutOfRange, "No cleaning before 8am")
 	}
-	_, err := s.FDialServer(ctx, "printer")
+	conn, err := s.FDialServer(ctx, "printer")
 	if err != nil {
 		return nil, status.Errorf(codes.Unavailable, "printer is unavailable (%v), assuming office is on shutdown", err)
 	}
+	conn.Close()
 
 	config, err := s.loadConfig(ctx)
 	if err != nil {
